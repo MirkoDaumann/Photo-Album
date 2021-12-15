d9eb730e88afeefb9ef91b2859ad2ceba34c78ab
Atrastea <mirko_daumann@web.de>
2021-12-15 13:51:55 +0100

    Cleaned up base-application.
    Removed all unused autogenerated code from the app.
    Prefixed the App accordingly.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pa-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Photo-Album';
 }
